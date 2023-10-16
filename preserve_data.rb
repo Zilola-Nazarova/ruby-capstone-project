a5ffb91c6c78e39257f4d9c139cafe26085f57a7
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-16 21:23:07 +0500

    Import necessary files to app.rb

---
+++ preserve_data.rb
@@ -0,0 +1,7 @@
+require './preserve_data'
+require './load_data'
+
+# require './options/[Modules created by Essohanam]'
+# require './options/[Modules created by Suleiman]'
+# require './options/[Modules created by Zilola]'
+require './options/example'
