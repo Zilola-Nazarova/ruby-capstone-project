5d916ae2d34c2b3834efecdfaff790e086a12760
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-16 21:18:10 +0500

    Add regexp to prevent strings being converted to zero

--- main.rb
+++ main.rb
@@ -22,9 +22,11 @@ def main
   puts "Welcome to 'Catalog of my things' App!"
   show_options
   loop do
-    option = gets.chomp.to_i
+    option = gets.chomp
+    option = option.to_i if option =~ /^\d+$/
     if (0..12).include?(option)
       app.call_option(option)
+
       show_options
     else
       puts "\nChoose a number between 0 and 12"
