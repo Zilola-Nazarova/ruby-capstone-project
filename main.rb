a4111d2a6779bfa6cc55e1d6b1ccaad95a141539
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-16 20:56:57 +0500

    Create Greeting message

---
+++ main.rb
@@ -0,0 +1,16 @@
+def show_options
+  puts "Please choose an option by entering a number:
+1 - List all books
+2 - List all music albums
+3 - List all movies
+4 - List of games
+5 - List all genres
+6 - List all labels
+7 - List all authors
+8 - List all sources
+9 - Add a book
+10 - Add a music album
+11 - Add a movie
+12 - Add a game
+0 - Exit"
+end
