c31cc021966f8b627c7eaf0abfd734f2c4a1a5e3
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-16 21:23:38 +0500

    Create App class and include necessary modules to it

--- preserve_data.rb
+++ preserve_data.rb
@@ -5,3 +5,13 @@ require './load_data'
 # require './options/[Modules created by Suleiman]'
 # require './options/[Modules created by Zilola]'
 require './options/example'
+
+class App
+  # include [Modules created by Essohanam]
+  # include [Modules created by Suleiman]
+  # include [Modules created by Zilola]
+  include Example
+
+  include PreserveData
+  include LoadData
+end
