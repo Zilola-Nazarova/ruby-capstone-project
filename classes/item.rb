8ed1e22764c590a3a6e2c77fbe9ef8f2e1543483
GhostEsso <lilipitaham@gmail.com>
2023-10-16 18:40:47 +0000

    item.rb updated

--- classes/item.rb
+++ classes/item.rb
@@ -1,28 +1,22 @@
 class Item
-  attr_accessor :genre, :source, :label, :publish_date
-  attr_reader :id, :archived
-
-  def initialize(genre, source, label, publish_date, archived)
-    @id = Random.rand(1..1000)
-    @genre = genre
-    @source = source
-    @label = label
-    @publish_date = publish_date
-    @archived = archived
+    attr_accessor :genre, :author, :source, :label, :publish_date
+    attr_reader :id, :archived
+  
+    def initialize(publish_date, archived: false)
+      @id = rand(1..1000)
+      @publish_date = publish_date
+      @archived = archived
+      @label = nil
+      @genre = nil
+      @author = nil
+    end
+  
+    def can_be_archived?
+      (Time.now - @publish_date.to_time) > 10 * 365 * 24 * 60 * 60
+    end
+  
+    def move_to_archive
+      @archived = true if can_be_archived?
+    end
   end
-
-  private
-
-  def can_be_archived?
-    diff_years = Date.today.year - @publish_date.year
-    diff_years >= 10
-  end
-
-  public
-
-  def move_to_archive
-    return unless @can_be_archived
-
-    true
-  end
-end
+  
