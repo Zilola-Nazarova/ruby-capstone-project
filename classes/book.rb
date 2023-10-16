f2cc44139292ea181e5579cba9e82a379683b875
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-16 22:14:35 +0500

    Add more parameters that are specific to the Book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,7 +1,9 @@
 require './classes/item'
 
 class Book < Item
-  def initialize(publish_date, archived: false)
+  def initialize(publisher, cover_state, publish_date, archived: false)
     super(publish_date, archived: archived)
+    @publisher = publisher
+    @cover_state = cover_state
   end
 end
