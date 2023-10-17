4e8e50dc597a822ad8a7433ccbde5a18fb2f9f57
GhostEsso <lilipitaham@gmail.com>
2023-10-17 14:59:37 +0000

    databse for genres & music album

--- database/schema.sql
+++ database/schema.sql
@@ -13,3 +13,22 @@ CREATE TABLE label(
   color varchar(120),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE
+    genres (
+        id INT GENERATED ALWAYS AS IDENTITY,
+        name VARCHAR(255) NOT NULL,
+        PRIMARY KEY(id)
+);
+
+CREATE TABLE music_albums (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(255) NOT NULL,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL,
+    on_spotify BOOLEAN NOT NULL,
+    item_id INT DEFAULT NULL,
+    -- Foreign key that references the item table
+    PRIMARY KEY(id),
+    CONSTRAINT FK_item FOREIGN KEY (item_id) REFERENCES item (id)
+);
