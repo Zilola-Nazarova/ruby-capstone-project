ee4d9c8fa6e21aa5239d1341318743e08318cd5a
suleiman gacheru <dretrevor8@gmail.com>
2023-10-17 15:58:12 +0300

    modify properties

--- classes/game.rb
+++ classes/game.rb
@@ -1,10 +1,7 @@
-class Game < Item
-  # Attributes
+class GameAttributes
   attr_accessor :genre_id, :author_id, :source_id, :label_id, :title, :multiplayer, :last_played_at
 
-  # Methods
-  def initialize(genre_id, author_id, source_id, label_id, title, multiplayer, last_played_at, publish_date, archived: false)
-    super(publish_date, archived: archived)
+  def initialize(genre_id, author_id, source_id, label_id, title, multiplayer, last_played_at)
     @genre_id = genre_id
     @author_id = author_id
     @source_id = source_id
@@ -12,6 +9,13 @@ class Game < Item
     @title = title
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+end
+
+class Game < Item
+  def initialize(attributes, publish_date, archived: false)
+    super(publish_date, archived: archived)
+    @attributes = attributes
   end
 
   def can_be_archived?
