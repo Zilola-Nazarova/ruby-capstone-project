a933ca219e801e32acb38a1ca0e4122ee088c069
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-17 14:32:32 +0500

    Implement list_labels method

--- options/list_labels.rb
+++ options/list_labels.rb
@@ -1,5 +1,10 @@
 module ListLabels
   def list_labels
-    puts "\nLIST OF THE LABELS\n"
+    puts "\nLABELS LIST:\n"
+    if @labels.empty?
+      puts 'No labels found'
+    else
+      puts "#{i + 1}) Title: \"#{label.title}\", Color: #{label.color}, ID: #{label.id}"
+    end
   end
 end
