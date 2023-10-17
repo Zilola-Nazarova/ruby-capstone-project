686d799e0f303084580744a1f64004aa7d31c1e7
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-17 10:44:09 +0500

    Add test cases to check constructor and attributes

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -5,4 +5,28 @@ describe Label do
     @label = Label.new 'Title', 'Color'
     @item = Item.new '2022-12-12'
   end
+
+  context '#new' do
+    it 'should take two parameters and return a Label object' do
+      expect(@label).to be_an_instance_of Label
+    end
+
+    it 'item should have no label' do
+      expect(@item.label).to be nil
+    end
+  end
+
+  context 'should have correct attributes (title, color, items)' do
+    it 'should return the correct title' do
+      expect(@label.title).to eq 'Title'
+    end
+
+    it 'should return the correct color' do
+      expect(@label.color).to eq 'Color'
+    end
+
+    it 'should have 0 items in @items array' do
+      expect(@label.items.length).to eq 0
+    end
+  end
 end
