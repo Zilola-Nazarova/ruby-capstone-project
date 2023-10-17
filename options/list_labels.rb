976110244f07bc08c366af288218b5968f3ccb37
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-17 19:32:20 +0500

    Refactor code to make it more user friendly

--- options/list_labels.rb
+++ options/list_labels.rb
@@ -4,9 +4,13 @@ module ListLabels
     if @labels.empty?
       puts 'No labels found'
     else
-      @labels.each_with_index do |label, i|
-        puts "#{i + 1}) Title: \"#{label.title}\", Color: #{label.color}, ID: #{label.id}"
-      end
+      the_list
+    end
+  end
+
+  def the_list
+    @labels.each_with_index do |label, i|
+      puts "#{i + 1}) [#{label.class.name}] Title: \"#{label.title}\", Color: #{label.color}, ID: #{label.id}"
     end
   end
 end
