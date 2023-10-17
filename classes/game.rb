927dac32a4ae14318b724b3fa00d4a56d8b1a65f
suleiman gacheru <dretrevor8@gmail.com>
2023-10-17 19:45:50 +0300

    add properties

--- classes/game.rb
+++ classes/game.rb
@@ -1,24 +1,13 @@
-class GameAttributes
-  attr_accessor :genre_id, :author_id, :source_id, :label_id, :title, :multiplayer, :last_played_at
-
-  def initialize(genre_id, author_id, source_id, label_id, title, multiplayer, last_played_at)
-    @genre_id = genre_id
-    @author_id = author_id
-    @source_id = source_id
-    @label_id = label_id
-    @title = title
-    @multiplayer = multiplayer
-    @last_played_at = last_played_at
-  end
-end
+require_relative 'item'
 
 class Game < Item
-  def initialize(attributes, publish_date, archived: false)
-    super(publish_date, archived: archived)
-    @attributes = attributes
-  end
+  # Attributes
+  attr_accessor :multiplayer, :last_played_at
 
-  def can_be_archived?
-    false
+  # Methods
+  def initialize(multiplayer, last_played_at, publish_date, archived: false)
+    super(publish_date, archived: archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
 end
