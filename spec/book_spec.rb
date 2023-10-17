f2b6437dab17bd3c97d7d1b857be40898a950e1b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-17 10:23:45 +0500

    Initialize variables for Book tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,5 +1,11 @@
 require './classes/book'
 
 describe Book do
-
+  before :all do
+    @book = Book.new 'Publisher', 'good', '2022-12-12'
+    @new_book_good = Book.new 'Publisher', 'good', '2022-12-12'
+    @old_book_good = Book.new 'Publisher', 'good', '1990-12-12'
+    @new_book_bad = Book.new 'Publisher', 'BaD', '2022-12-12'
+    @old_book_bad = Book.new 'Publisher', 'BaD', '1990-12-12'
+  end
 end
