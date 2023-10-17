7248f96c2483df01930ddad57e1bf7ee238913c8
suleiman-gacheru <dretrevor8@gmail.com>
2023-10-17 05:53:25 +0300

    solve rubocop indentations

--- classes/item.rb
+++ classes/item.rb
@@ -11,13 +11,17 @@ class Item
     @archived = archived
   end
 
+  private
+
   def can_be_archived?
     diff_years = Date.today.year - @publish_date.year
     diff_years >= 10
   end
 
+  public
+
   def move_to_archive
-    return unless can_be_archived?
+    return unless @can_be_archived
 
     true
   end
