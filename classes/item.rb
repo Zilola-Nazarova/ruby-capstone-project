0afa7bed4cde35c78f5578b5f797b053d6f0f36b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-17 11:00:21 +0500

    Fix rubocop errors (white spaces and indentation)

--- classes/item.rb
+++ classes/item.rb
@@ -1,22 +1,21 @@
 class Item
-    attr_accessor :genre, :author, :source, :label, :publish_date
-    attr_reader :id, :archived
-  
-    def initialize(publish_date, archived: false)
-      @id = rand(1..1000)
-      @publish_date = publish_date
-      @archived = archived
-      @label = nil
-      @genre = nil
-      @author = nil
-    end
-  
-    def can_be_archived?
-      (Time.now - @publish_date.to_time) > 10 * 365 * 24 * 60 * 60
-    end
-  
-    def move_to_archive
-      @archived = true if can_be_archived?
-    end
+  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_reader :id, :archived
+
+  def initialize(publish_date, archived: false)
+    @id = rand(1..1000)
+    @publish_date = publish_date
+    @archived = archived
+    @label = nil
+    @genre = nil
+    @author = nil
   end
-  
+
+  def can_be_archived?
+    (Time.now - @publish_date.to_time) > 10 * 365 * 24 * 60 * 60
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+end
