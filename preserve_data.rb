ee33d88ba5b89605d1fcffa50a053311422d9dfb
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-17 19:05:07 +0500

    Implement save_books and save_labels methods

--- preserve_data.rb
+++ preserve_data.rb
@@ -2,27 +2,38 @@ require 'json'
 
 module PreserveData
   def save_all_files
-    save_books # by Zilola
+    save_books
     # save_albums by Essohanam (change if necessary)
     # save_games by Suleiman (change if necessary)
     # save_genres by Essohanam (change if necessary)
-    save_labels # by Zilola
+    save_labels
     # save_authors by Suleiman (change if necessary)
   end
 
+
   def save_books
-    File.open('./files/books.json', 'w') {|file| file.truncate(0) }
-    @books.map do |book|
-      book_json = JSON.generate(book)
-      File.write('./files/books.json', "#{book_json}\n", mode: 'a')
+    books_json = @books.map do |book|
+      {
+        publisher: book.publisher,
+        cover_state: book.cover_state,
+        publish_date: book.publish_date,
+        archived: book.archived,
+        label: {
+          title: book.label.title,
+          color: book.label.color
+        }
+      }
     end
+    File.write('./files/books.json', JSON.pretty_generate(books_json))
   end
 
   def save_labels
-    File.open('./files/labels.json', 'w') {|file| file.truncate(0) }
-    @labels.map do |label|
-      label_json = JSON.generate(label)
-      File.write('./files/labels.json', "#{label_json}\n", mode: 'a')
+    labels_json = @labels.map do |label|
+      label_json = {
+        title: label.title,
+        color: label.color
+      }
     end
+    File.write('./files/labels.json', JSON.pretty_generate(labels_json))
   end
 end
