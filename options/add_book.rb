7e8b85c3502a55e436c2971a94f7f019e3e46bed
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-18 18:04:21 +0500

    Sync codes to add Book and add MusicAlbum, refactor add_props

--- options/add_book.rb
+++ options/add_book.rb
@@ -1,57 +1,34 @@
 require './classes/book'
 require './classes/label'
 require './options/list_labels'
+require './options/add_props'
 
 module AddBook
+  include AddProps
+
   def add_book
     puts 'The book was published by ...'
     publisher = gets.chomp.to_s
     puts "Describe a cover state of the book (e.g. 'good', 'bad')"
     cover_state = gets.chomp.to_s
     puts "When the book was published? Provide in format 'yyyy-mm-dd'"
-    loop do
-      publish_date = gets.chomp
-      if publish_date =~ /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/
-        @book = Book.new(publisher, cover_state, publish_date)
-        @books.push(@book)
-        puts "Book created successfully\n"
-        choose_label
-        break
-      elsif publish_date == 'exit'
-        puts 'Navigating back to menu'
-        return
-      else
-        puts "Provide the date in format 'yyyy-mm-dd' or type 'exit' to go to main menu"
-      end
-    end
-  end
 
-  def choose_label
-    puts "\nAdd a Label to the Book:\n0) Create a new label"
-    the_list unless @labels.empty?
     loop do
-      label_index = gets.chomp.to_i
-      if label_index.zero?
-        create_label
-        break
-      elsif (1..(@labels.length)).include?(label_index)
-        @book.label = @labels[label_index - 1]
-        puts "Label was assigned to the Book\n"
-        break
-      else
-        puts "Please provide a valide input (0-#{@labels.length})"
-      end
-    end
-  end
+      @publish_date = gets.chomp
+      break if @publish_date =~ /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/
 
-  def create_label
-    puts 'Label title:'
-    title = gets.chomp.to_s
-    puts 'Label color:'
-    color = gets.chomp.to_s
-    label = Label.new(title, color)
-    @labels.push(label) unless @labels.include?(label)
-    @book.label = label
-    puts "Label was created and assigned to the Book!\n"
+      return if @publish_date == 'exit'
+
+      puts "Provide the date in format 'yyyy-mm-dd' or type 'exit' to go to main menu"
+    end
+
+    book = Book.new(publisher, cover_state, @publish_date)
+    @books.push(book)
+
+    choose_label(book)
+    choose_genre(book)
+    # choose_author(book)
+
+    puts "Book created successfully!\n"
   end
 end
--- options/add_musicalbum.rb
+++ options/add_musicalbum.rb
@@ -1,24 +1,36 @@
+require './options/add_props'
 require './classes/musicalbum'
-require './classes/genre'
-require './options/list_music_albums'
 
-def add_music_album
-  puts 'Enter album label:'
-  label = gets.chomp
+module AddMusicAlbum
+  include AddProps
 
-  puts 'Enter album genre:'
-  genre_name = gets.chomp
-  genre = @genres.find { |g| g.name == genre_name } || Genre.new(genre_name)
-  @genres << genre unless @genres.include?(genre)
-  puts 'Enter publish date (yyyy-mm-dd):'
-  publish_date = gets.chomp
-  puts 'Available on Spotify? (y/n):'
-  on_spotify = gets.chomp.downcase == 'y'
+  def add_music_album
+    puts 'Enter publish date (yyyy-mm-dd):'
 
-  album = MusicAlbum.new(label, genre, publish_date, on_spotify, nil)
-  attribute_game_to_author(album)
-  @music_albums << album
-  genre.add_item(album)
+    loop do
+      @publish_date = gets.chomp
+      break if @publish_date =~ /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/
 
-  puts 'Album added successfully!'
+      puts "Provide the date in format 'yyyy-mm-dd'"
+    end
+
+    puts 'Available on Spotify? (Y/y/N/n):'
+
+    loop do
+      @on_spotify = gets.chomp
+      break if @on_spotify =~ /^[YyNn]/
+
+      puts 'Provide valid input (Y/y/N/n)'
+    end
+
+    @on_spotify = @on_spotify.downcase == 'y'
+    album = MusicAlbum.new(@publish_date, @on_spotify)
+    @music_albums << album
+
+    choose_label(album)
+    choose_genre(album)
+    # choose_author(album)
+
+    puts "Album added successfully!\n"
+  end
 end
