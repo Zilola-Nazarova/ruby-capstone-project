f2a46dce34a989a2bb7943d5eddae2ddb1d4ccdb
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-18 18:00:10 +0500

    Import Essohanam's files to app.rb

--- app.rb
+++ app.rb
@@ -1,15 +1,19 @@
 require './preserve_data'
 require './load_data'
 
-# require './options/[Modules created by Essohanam]'
 # require './options/[Modules created by Suleiman]'
 require './options/list_books'
 require './options/list_labels'
 require './options/add_book'
+require './options/list_genres'
+require './options/list_music_album'
+require './options/add_musicalbum'
 
 class App
-  # include [Modules created by Essohanam]
   # include [Modules created by Suleiman]
+  include AddMusicAlbum
+  include ListMusicAlbums
+  include ListGenres
   include AddBook
   include ListBooks
   include ListLabels
@@ -19,25 +23,25 @@ class App
 
   OPTIONS_HASH = {
     1 => :list_books,
-    # 2 => :[Method to List all music albums created by Essohanam],
+    2 => :list_music_albums,
     # 3 => :Method to List all movies We don not have this option,
-    # 4 => :Method to List of games created by Suleiman,
-    # 5 => :Method to List all genres created by Essohanam,
+    4 => :list_games,
+    5 => :list_genres,
     6 => :list_labels,
     # 7 => :Method to List all authors created by Suleiman,
     # 8 => :Method to List all sources We don not have this option,
-    9 => :add_book
-    # 10 => :Method to Add a music album created by Essohanam,
+    9 => :add_book,
+    10 => :add_music_album
     # 11 => :Method to Add a movie We don not have this option,
     # 12 => :Method to Add a game created by Suleiman
   }.freeze
 
   # change names of your variables if necessary
-  attr_accessor :books, :albums, :games, :genres, :labels, :authors
+  attr_accessor :books, :music_albums, :games, :genres, :labels, :authors
 
   def initialize
     @books = [] # by Zilola
-    @albums = [] # by Essohanam (change if necessary)
+    @music_albums = [] # by Essohanam (change if necessary)
     @games = [] # by Suleiman (change if necessary)
     @genres = [] # by Essohanam (change if necessary)
     @labels = [] # by Zilola
