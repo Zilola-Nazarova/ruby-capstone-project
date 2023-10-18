4b9c4b52e88626096ed06affef2a0d7339a56c7f
suleiman gacheru <dretrevor8@gmail.com>
2023-10-18 20:05:39 +0300

    modfiy preserve data , loaddata

--- classes/author.rb
+++ classes/author.rb
@@ -2,8 +2,8 @@ class Author
   attr_reader :id, :items
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
-    @id = Random.rand(1..1000)
+  def initialize(first_name, last_name, id = Random.rand(1..1000))
+    @id = id
     @first_name = first_name
     @last_name = last_name
     @items = []
