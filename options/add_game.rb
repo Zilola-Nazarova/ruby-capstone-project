8767c1043ba6dcde860ece0cf1cabd90b48a7b48
suleiman gacheru <dretrevor8@gmail.com>
2023-10-19 12:59:42 +0300

    update team-files

--- options/add_game.rb
+++ options/add_game.rb
@@ -1,37 +1,44 @@
-require_relative './item'
 require 'date'
+require './classes/game'
 
 module AddGame
   include AddProps
 
   def add_game
     puts 'Is the game multiplayer? (yes/no)'
-    multiplayer_input = gets.chomp.downcase
-    multiplayer = multiplayer_input == 'yes'
+
+    loop do
+      @multiplayer_input = gets.chomp.downcase
+      break if @multiplayer_input =~ /^(?:yes|no)$/
+
+      puts 'Provide valid input (yes/no)'
+    end
+
+    multiplayer = @multiplayer_input == 'yes'
 
     puts 'When was the game last played? Provide in format \'yyyy-mm-dd\''
-    loop do
-      last_played_date = gets.chomp
-      break if last_played_date =~ /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/
-
-      return if last_played_date == 'exit'
-
-      puts "Provide the date in format 'yyyy-mm-dd' or type 'exit' to go to the main menu"
-    end
+    date_prompt(@last_played_date)
 
     puts 'When was the game published? Provide in format \'yyyy-mm-dd\''
-    loop do
-      publish_date = gets.chomp
-      break if publish_date =~ /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/
+    date_prompt(@publish_date)
 
-      return if publish_date == 'exit'
-
-      puts "Provide the date in format 'yyyy-mm-dd' or type 'exit' to go to the main menu"
-    end
-
-    game = Game.new(multiplayer, Date.parse(last_played_date), publish_date)
+    game = Game.new(multiplayer, @last_played_date, @publish_date)
     @games.push(game)
 
     choose_label(game)
+
+    puts "Game added successfully!\n"
+  end
+
+  def date_prompt(date)
+    loop do
+      date = gets.chomp
+      break if date =~ /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/
+
+      return if date == 'exit'
+
+      puts "Provide the date in format 'yyyy-mm-dd' or type 'exit' to go to the main menu"
+    end
+    date
   end
 end
